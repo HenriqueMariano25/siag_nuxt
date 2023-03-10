eabe0f0b2bf8f1dbcc3fb566223ccc10874b0f62
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-03-10 07:34:55 -0300

    Adiciona sp/bh. moi e mod por setor no relatorio de Gente

--- pages/Administracao/Gente/index.vue
+++ pages/Administracao/Gente/index.vue
@@ -89,21 +89,49 @@ export default {
                 valMoi[cont] = ((((moiPos * 100) / moiGer) - (moiNeg * 100) / moiGer)).toFixed(0) + '%'
                 mediaMoi += parseFloat(((moiPos * 100) / moiGer) - (moiNeg * 100) / moiGer)
 
-
                 for(let set of Object.keys(dados[ano][gr][per]['setores'])){
                   if(Object.keys(setores).includes(set) === false){
                    setores[set] = [set]
                   }
 
-                  let setPos = dados[ano][gr][per]['setores'][set]['positivo']
-                  let setNeg = dados[ano][gr][per]['setores'][set]['negativo']
-                  let setGeral = dados[ano][gr][per]['setores'][set]['geral']
+                  if (Object.keys(setores).includes(set + ' - SP/BH') === false) {
+                    setores[set + ' - SP/BH'] = [set + ' - SP/BH']
+                  }
+
+                  if (Object.keys(setores).includes(set + ' - MOI') === false) {
+                    setores[set + ' - MOI'] = [set + ' - MOI']
+                  }
+
+                  if (Object.keys(setores).includes(set + ' - MOD') === false) {
+                    setores[set + ' - MOD'] = [set + ' - MOD']
+                  }
+
+                  let setPos = parseInt(dados[ano][gr][per]['setores'][set]['positivo'])
+                  let setNeg = parseInt(dados[ano][gr][per]['setores'][set]['negativo'])
+                  let setGeral = parseInt(dados[ano][gr][per]['setores'][set]['geral'])
 
                   setores[set][cont] = ((((setPos * 100) / setGeral) - (setNeg * 100) / setGeral)).toFixed(0) + '%'
+
+                  let setSpBhPos = parseInt(dados[ano][gr][per]['setores'][set]['spBh']['positivo'])
+                  let setSpBhNeg = parseInt(dados[ano][gr][per]['setores'][set]['spBh']['negativo'])
+                  let setSpBhGeral = parseInt(dados[ano][gr][per]['setores'][set]['spBh']['geral'])
+
+                  let setMoiPos = parseInt(dados[ano][gr][per]['setores'][set]['moi']['positivo'])
+                  let setMoihNeg = parseInt(dados[ano][gr][per]['setores'][set]['moi']['negativo'])
+                  let setMoiGeral = parseInt(dados[ano][gr][per]['setores'][set]['moi']['geral'])
+
+                  let setModPos = parseInt(dados[ano][gr][per]['setores'][set]['mod']['positivo'])
+                  let setModNeg = parseInt(dados[ano][gr][per]['setores'][set]['mod']['negativo'])
+                  let setModGeral = parseInt(dados[ano][gr][per]['setores'][set]['mod']['geral'])
+
+                  setores[set + ' - SP/BH'][cont] = setSpBhGeral > 0 ? ((((setSpBhPos * 100) / setSpBhGeral) - (setSpBhNeg * 100) / setSpBhGeral)).toFixed(0) + '%' : "0%"
+                  setores[set + ' - MOI'][cont] = setMoiGeral > 0 ? ((((setMoiPos * 100) / setMoiGeral) - (setMoihNeg * 100) / setMoiGeral)).toFixed(0) + '%' : "0%"
+                  setores[set + ' - MOD'][cont] = setModGeral > 0 ? ((((setModPos * 100) / setModGeral) - (setModNeg * 100) / setModGeral)).toFixed(0) + '%' : "0%"
                 }
 
                 cont++
               }
+
               valGeral[cont] = (media/Object.keys(dados[ano][gr]).length).toFixed(0) + '%'
               valSpBh[cont] = (mediaSpBh/Object.keys(dados[ano][gr]).length).toFixed(0) + '%'
               valMod[cont] = (mediaMod/Object.keys(dados[ano][gr]).length).toFixed(0) + '%'
@@ -123,8 +151,18 @@ export default {
 
             let nomeSetores = Object.keys(setores)
 
+            let contPulaLinha = 0
             for(let z = 0; z <= nomeSetores.length - 1; z++){
                 worksheet.addRow( setores[nomeSetores[z]]);
+
+                if(contPulaLinha === 3){
+                  contPulaLinha = 0
+                  worksheet.addRow();
+                }else{
+                  contPulaLinha++
+                }
+
+
             }
           }
 
