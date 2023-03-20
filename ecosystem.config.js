e90c90e0b662d7dae05c48b9994932d0e9ff7df0
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-03-20 11:22:35 -0300

    Adiciona nova configuração do deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'Siag',
-      port: '82',
+      name: 'NuxtAppName',
       exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
   ]
