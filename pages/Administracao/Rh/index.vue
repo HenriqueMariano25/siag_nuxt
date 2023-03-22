54eedf038e30970705e8ef2598257a79cae3d1bb
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-03-22 16:31:24 -0300

    Esconde modulos que não estão prontos ainda

--- pages/Administracao/Rh/index.vue
+++ pages/Administracao/Rh/index.vue
@@ -10,7 +10,8 @@
             d="M5.082 14.254a8.287 8.287 0 00-1.308 5.135 9.687 9.687 0 01-1.764-.44l-.115-.04a.563.563 0 01-.373-.487l-.01-.121a3.75 3.75 0 013.57-4.047zM20.226 19.389a8.287 8.287 0 00-1.308-5.135 3.75 3.75 0 013.57 4.047l-.01.121a.563.563 0 01-.373.486l-.115.04c-.567.2-1.156.349-1.764.441z"/>
         </svg>
       </ButtonNavegacao>
-      <ButtonNavegacao titulo="Contratação" cor="bg-[#264653]" link="/administracao/rh/contratacao">
+      <ButtonNavegacao titulo="Contratação" cor="bg-[#264653]" link="/administracao/rh/contratacao"
+                       v-if="$auth.user.permissoes.includes('administracao_contratacao')">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8">
           <path
             d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"/>
