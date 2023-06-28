c18068a6a395004dbc25811556fcddd2d9585ca6
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-06-28 15:41:51 -0300

    Adicionar gráfico com os agendamentos Aprovados e Pendentes na Tela Inicial de Hora Extra

--- pages/Administracao/Rh/HoraExtra/index.vue
+++ pages/Administracao/Rh/HoraExtra/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full grid  h-fit">
+  <div class="w-full grid  h-fit gap-2">
     <div class="border grid grid-cols-4 space-x-3 border-1 shadow border-gray-300 p-2 rounded bg-white ">
       <ButtonNavegacao titulo="Agendar" cor="bg-[#264653]" link="/administracao/rh/horaExtra/agendar"
                        v-if="$auth.user.permissoes.includes('horaExtra_rh')">
@@ -14,6 +14,16 @@
         <img src="@/assets/icons/cog-w.svg" alt="" class="w-8 h-8">
       </ButtonNavegacao>
     </div>
+    <div class="bg-white items-center flex" style="height: calc(100vh - 165px)">
+      <div ref="graAgendamento" class="flex w-full"></div>
+<!--      <ApexChart-->
+<!--        class=""-->
+<!--        type="pie"-->
+<!--        red="graAgendamento"-->
+<!--        :options="opcoesAgendamento"-->
+<!--        :series="valoresAgendamento"-->
+<!--        id="graAgendamento" />-->
+    </div>
   </div>
 </template>
 
@@ -21,8 +31,116 @@
 import ButtonNavegacao from "~/components/Shared/ButtonNavegacao.vue";
 
 export default {
-  components: {ButtonNavegacao}
+  components: {ButtonNavegacao},
+  data() {
+    return {
+      valoresAgendamento: [],
+      opcoesAgendamento: {
+          chart: {
+            type: 'bar',
+            height: 500,
+            stacked: true,
+            toolbar: {
+              show: true
+            },
+            zoom: {
+              enabled: true
+            }
+          },
+          responsive: [{
+            breakpoint: 480,
+            options: {
+              legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+              }
+            }
+          }],
+        title: {
+          text: 'Agendamentos de HE aprovadas e pendentes',
+          offsetY: 0,
+          align: 'top',
+          style: {
+            color: '#444',
+            fontSize: '18px',
+            fontWeight: 'bold',
+          }
+        },
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              borderRadius: 10,
+              dataLabels: {
+                total: {
+                  enabled: true,
+                  style: {
+                    fontSize: '13px',
+                    fontWeight: 900
+                  }
+                }
+              }
+            },
+          },
+          xaxis: {
+            categories: [],
+          },
+          legend: {
+            position: 'top',
+            // offsetY: 40
+          },
+          fill: {
+            opacity: 1
+          }
+      },
+    };
+  },
+  async created() {
+    await this.buscarAgendamentos()
+  },
+  methods: {
+    async buscarAgendamentos() {
+      let resp = await this.$axios.$get('/hora_extra/graficos/todos_agendamentos')
 
+      console.log(resp)
+
+      if(!resp.falha){
+
+
+        let { datas, aprovados, pendentesGestor, pendentesSite } = resp.dados
+
+        this.valoresAgendamento = [{
+          name: 'Aprovados',
+          data: [...aprovados]
+        }, {
+          name: 'Sem Aprov. Gestor',
+          data: [...pendentesGestor]
+        }, {
+          name: 'Sem Aprov. Site Manager',
+          data: [...pendentesSite]
+        }]
+
+        this.opcoesAgendamento.xaxis.categories = [...datas]
+
+        let valores = this.valoresAgendamento
+        let opcoes = Object.assign({}, this.opcoesAgendamento)
+
+        const chartContainer = this.$refs.graAgendamento;
+
+        const chart = new ApexCharts(chartContainer, {
+          ...opcoes,
+
+          series: valores,
+        });
+
+        await chart.render();
+      }
+
+
+
+
+    }
+  }
 }
 </script>
 
