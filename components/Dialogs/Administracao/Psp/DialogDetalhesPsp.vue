7592c7dedb4a8f0e07dfe37eb51c161b80f7293c
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-09-06 13:03:15 -0300

    Melhora impressão dos detalhes da PSP

--- components/Dialogs/Administracao/Psp/DialogDetalhesPsp.vue
+++ components/Dialogs/Administracao/Psp/DialogDetalhesPsp.vue
@@ -11,8 +11,18 @@
 				<div
 					class="grid grid-cols-1 divide-y divide-gray-300 text-lg w-full"
 					v-if="psp">
-          <div class="border-b-2 border-black hidden print:flex">
-            <span class="text-3xl"><strong>Gestão de Viagem</strong></span>
+
+          <div class="!border-b-2 !border-b-black w-full flex-col hidden print:flex">
+            <div class="justify-between flex w-full items-center mb-1">
+              <img
+                src="@/assets/img/logogrande.png"
+                alt=""
+                class="w-[250px]" />
+              <span>{{ $dayjs().format("DD/MM/YYYY HH:mm") }}</span>
+            </div>
+            <div class="text-center">
+              <span class="text-3xl"><strong>Gestão de Viagem</strong></span>
+            </div>
           </div>
 					<div class="flex justify-between w-full">
 						<span><strong>Código: </strong>{{ ("00000" + psp.id).slice(-5) }}</span>
@@ -57,7 +67,7 @@
 					</div>
 					<span> <strong>Origem: </strong>{{ psp.origem }} </span>
 					<span> <strong>Destino: </strong>{{ psp.destino }} </span>
-					<span> <strong>Transporte: </strong>{{ psp.transporte }} </span>
+					<span> <strong>Transporte: </strong>{{ psp.meio_transporte }} </span>
 					<span> <strong>Motivo: </strong>{{ psp.motivo }} </span>
 					<span>
 						<strong>Centro de Custo: </strong>
@@ -71,68 +81,80 @@
 						<strong>Solicitado por: </strong>{{ psp.criado_por ? psp.criado_por.nome : "" }}
 					</span>
 					<span class="titulo"><strong>Aprovações</strong></span>
-					<div class="grid grid-cols-2 w-full divide-x divide-gray-400" v-if="psp.aprovador_gaf && psp.aprovacao_gaf">
+					<div
+						class="grid grid-cols-2 w-full divide-x divide-gray-400"
+						v-if="psp.aprovador_gaf && psp.aprovacao_gaf">
 						<span class="grow">
-							<strong>Aprovador GAF: </strong>
+							<strong>Aprov. GAF: </strong>
 							{{ psp.aprovador_gaf ? psp.aprovador_gaf.nome : "" }}
 						</span>
 						<span class="grow">
-							<strong>Data da aprovação: </strong>
+							<strong>Data: </strong>
 							{{ $dayjs(psp.data_aprovacao_gaf).format("DD/MM/YYYY") }}
 						</span>
 					</div>
-          <div class="grid grid-cols-2 w-full divide-x divide-gray-400" v-if="psp.aprovador_rh && psp.aprovacao_rh">
+					<div
+						class="grid grid-cols-2 w-full divide-x divide-gray-400"
+						v-if="psp.aprovador_rh && psp.aprovacao_rh">
 						<span class="grow">
-							<strong>Aprovador RH: </strong>
+							<strong>Aprov. RH: </strong>
 							{{ psp.aprovador_rh ? psp.aprovador_rh.nome : "" }}
 						</span>
-            <span class="grow">
-							<strong>Data da aprovação: </strong>
+						<span class="grow">
+							<strong>Data: </strong>
 							{{ $dayjs(psp.data_aprovacao_rh).format("DD/MM/YYYY") }}
 						</span>
-          </div>
-          <div class="grid grid-cols-2 w-full divide-x divide-gray-400" v-if="psp.aprovador_coordenador && psp.aprovacao_coordenador">
+					</div>
+					<div
+						class="grid grid-cols-2 w-full divide-x divide-gray-400"
+						v-if="psp.aprovador_coordenador && psp.aprovacao_coordenador">
 						<span class="grow">
-							<strong>Aprovador Coordenador: </strong>
+							<strong>Aprov. Coord.: </strong>
 							{{ psp.aprovador_coordenador ? psp.aprovador_coordenador.nome : "" }}
 						</span>
-            <span class="grow">
-							<strong>Data da aprovação: </strong>
+						<span class="grow">
+							<strong>Data: </strong>
 							{{ $dayjs(psp.data_aprovacao_coordenador).format("DD/MM/YYYY") }}
 						</span>
-          </div>
-          <div class="grid grid-cols-2 w-full divide-x divide-gray-400"
-               v-if="psp.aprovador_gerente_area && psp.aprovacao_gerente_area">
+					</div>
+					<div
+						class="grid grid-cols-2 w-full divide-x divide-gray-400"
+						v-if="psp.aprovador_gerente_area && psp.aprovacao_gerente_area">
 						<span class="grow">
-							<strong>Aprovador Gerente Área: </strong>
+							<strong>Aprov. Gerente: </strong>
 							{{ psp.aprovador_gerente_area ? psp.aprovador_gerente_area.nome : "" }}
 						</span>
-            <span class="grow">
-							<strong>Data da aprovação: </strong>
+						<span class="grow">
+							<strong>Data: </strong>
 							{{ $dayjs(psp.aprovacao_gerente_area).format("DD/MM/YYYY") }}
 						</span>
-          </div>
+					</div>
 				</div>
 			</div>
 		</template>
-    <template v-slot:rodape-btn-direito>
-      <div class="flex">
-        <BotaoPadrao texto="imprimir" @clique="imprimir()">
-          <img src="@/assets/icons/printer-b.svg" alt="" class="w-7 h-7">
-        </BotaoPadrao>
-      </div>
-    </template>
+		<template v-slot:rodape-btn-direito>
+			<div class="flex">
+				<BotaoPadrao
+					texto="imprimir"
+					@clique="imprimir()">
+					<img
+						src="@/assets/icons/printer-b.svg"
+						alt=""
+						class="w-7 h-7" />
+				</BotaoPadrao>
+			</div>
+		</template>
 	</BaseDialog>
 </template>
 
 <script>
 	import BaseDialog from "~/components/Shared/BaseDialog.vue"
-  import BotaoPadrao from "~/components/Ui/Buttons/BotaoPadrao.vue";
+	import BotaoPadrao from "~/components/Ui/Buttons/BotaoPadrao.vue"
 
 	export default {
 		name: "DialogDetalhesPsp",
 		components: {
-      BotaoPadrao,
+			BotaoPadrao,
 			BaseDialog,
 		},
 		props: {
@@ -164,9 +186,9 @@
 					this.carregando = false
 				}
 			},
-      imprimir(){
-        window.print()
-      }
+			imprimir() {
+				window.print()
+			},
 		},
 	}
 </script>
