386b4ba46f8d63e3f3102183f00a2874c76dae31
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-10-02 09:44:10 -0300

    Prepara o SIAG para fazer login via usuario AG

---
+++ pages/Erro/SemSetor.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="w-full justify-center fixed flex items-center h-full">
+		<div
+			class="flex flex-col border-2 rounded border-gray-700 bg-slate-300 externa justify-center text-center p-2 gap-3">
+			<div class="flex w-full justify-center">
+				<img
+					src="@/assets/icons/alert-triangle-r.svg"
+					alt=""
+					class="w-[100px] h-[100px] flex" />
+			</div>
+			<div class="text-center">
+				<span
+					class="text-2xl"
+					v-if="$auth.user">
+					O usuário <strong>{{ $auth.user.usuario }}</strong> não tem um SETOR atribuído, favor
+					procurar o <i>ADMINSTRADOR</i> do SIAG.
+				</span>
+			</div>
+			<div>
+				<button
+					@click="mostrarDialogLogout = true"
+					class="bg-primaria-500 hover:bg-primaria-700 p-2 text-white text-xl w-[200px] font-bold">
+					SAIR
+				</button>
+			</div>
+		</div>
+		<DialogLogout
+			@cancelar="mostrarDialogLogout = false"
+			v-if="mostrarDialogLogout" />
+	</div>
+</template>
+
+<script>
+	import DialogLogout from "~/components/Dialogs/DialogLogout.vue"
+	import BotaoPadrao from "~/components/Ui/Buttons/BotaoPadrao.vue"
+
+	export default {
+		components: { BotaoPadrao, DialogLogout },
+		name: "SemSetor",
+		layout: "auth",
+		data() {
+			return {
+				mostrarDialogLogout: false,
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.externa {
+		width: 80vw;
+	}
+</style>
--- pages/Login.vue
+++ pages/Login.vue
@@ -1,12 +1,14 @@
 <template>
 	<div
 		class="flex fixed bg-cover bg-center bg-no-repeat w-full h-full bg-login justify-center items-center"
-    v-on:keyup.enter="login">
-		<div class="border border-gray-800 bg-gray-100/[.80] flex flex-col z-10 p-6 h-fit w-1/3 "
-         >
-      <div>
-        <img src="@/assets/img/logogrande.png" alt="" class="mb-3">
-      </div>
+		v-on:keyup.enter="login">
+		<div class="border border-gray-800 bg-gray-100/[.80] flex flex-col z-10 p-6 h-fit w-1/3">
+			<div>
+				<img
+					src="@/assets/img/logogrande.png"
+					alt=""
+					class="mb-3" />
+			</div>
 			<div class="font-bold text-center text-6xl mb-2 text-[#00152D]">
 				<h1>SIAG</h1>
 			</div>
@@ -16,34 +18,39 @@
 					placeholder="Ex: henrique.mariano"
 					label="Usuário"
 					v-model="usuario.usuario"
-
-          id="usuario"
-        />
+					id="usuario" />
 				<AppFormInput
 					type="password"
 					label="Senha"
-          id="senha"
+					id="senha"
 					v-model="usuario.senha" />
-        <div class="bg-red-200 text-red-700 p-1 border border-red-800" v-if="erro !== null">
-          <span v-if="erro === 'server_error'">Erro no servidor, entre em contato com o suporte!</span>
-          <span v-if="erro === 'invalid_data'">Usuário ou senha incorreto!</span>
-        </div>
+				<div
+					class="bg-red-200 text-red-700 p-1 border border-red-800"
+					v-if="erro !== null">
+					<span v-if="erro === 'server_error'"
+						>Erro no servidor, entre em contato com o suporte!</span
+					>
+					<span v-if="erro === 'invalid_data'">Usuário ou senha incorreto!</span>
+				</div>
+				<div class="text-sm bg-yellow-100 px-2 border border-yellow-300">
+					<span>
+						<strong>Observação: </strong>O usuário e a senha são os mesmos de acesso ao computador.
+					</span>
+				</div>
 				<div class="w-full">
 					<button
-            ref="inputUsuario"
+						ref="inputUsuario"
 						class="p-1 bg-primaria-700 text-white w-full rounded-sm"
-            v-on:keyup.enter="login"
+						v-on:keyup.enter="login"
 						@click.stop.prevent="login">
 						ENTRAR
 					</button>
 				</div>
 			</div>
-
 		</div>
-    <div class="mt-6 text-center text-lg absolute bottom-0 w-full bg-[#00152D] text-white py-1">
-      <span>Desenvolvido por: <strong>Henrique Mariano da Silva</strong></span>
-
-    </div>
+		<div class="mt-6 text-center text-lg absolute bottom-0 w-full bg-[#00152D] text-white py-1">
+			<span>Desenvolvido por: <strong>Henrique Mariano da Silva</strong></span>
+		</div>
 	</div>
 </template>
 
@@ -61,43 +68,49 @@
 					usuario: null,
 					senha: null,
 				},
-        erro: null
+				erro: null,
 			}
 		},
-    mounted() {
-      this.focusInput();
-      // this.$refs.inputUsuario.$el.focus();
-      // this.$nextTick(() => {
-        // This callback will only be called after the
-        // DOM has been updated
-        // this.$refs.inputUsuario.focus();
-      // });
-    },
+		mounted() {
+			this.focusInput()
+			// this.$refs.inputUsuario.$el.focus();
+			// this.$nextTick(() => {
+			// This callback will only be called after the
+			// DOM has been updated
+			// this.$refs.inputUsuario.focus();
+			// });
+		},
 		methods: {
-      focusInput() {
-        this.$refs.inputUsuario.focus();
-      },
+			focusInput() {
+				this.$refs.inputUsuario.focus()
+			},
 			async login() {
-        let {usuario, senha} = this.usuario
-        this.erro = null
+				let { usuario, senha } = this.usuario
+				this.erro = null
 
-        try {
-          let resp = await this.$auth.loginWith('local', {
-            data: {
-              usuario,
-              senha
-            },
-          })
+				try {
+					let resp = await this.$auth.loginWith("local", {
+						data: {
+							usuario,
+							senha,
+						},
+					})
 
-          await this.$router.push("/")
-        } catch (e) {
-          if (!e.response) {
-            this.erro = "server_error"
-          } else {
-            if (e.response && e.response.status === 401)
-              this.erro = "invalid_data"
-          }
-        }
+					let usuarioBuscado = resp.data.usuario
+					if (!usuarioBuscado.setor_id) {
+						await this.$router.push("/erro/SemSetor")
+					} else {
+						await this.$router.push("/")
+					}
+				} catch (e) {
+					console.log(e.response)
+
+					if (!e.response) {
+						this.erro = "server_error"
+					} else {
+						if (e.response && e.response.status === 401) this.erro = "invalid_data"
+					}
+				}
 			},
 		},
 	}
