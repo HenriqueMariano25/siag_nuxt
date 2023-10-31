b7337a5b2d05f7e39df7a145bc06bf886c690ea1
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-10-31 16:22:36 -0300

    Adicionar filtro por intervalo de dias que será usado para média do KPI de Contratacao

--- components/Graphic/Administracao/Rh/Contratação/GraficoDuracaoEtapa.vue
+++ components/Graphic/Administracao/Rh/Contratação/GraficoDuracaoEtapa.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="relative flex flex-col w-full ">
-    <div class="flex gap-2 bg-gray-300 px-2 w-full py-1 items-center print:hidden">
-      <BotaoPadrao
-        texto="Excel"
-        @clique="buscarKpiDuracao"
-        cor="bg-blue-300 hover:!bg-blue-400">
-        <img
-          src="@/assets/icons/excel-b.svg"
-          alt=""
-          class="w-7 h-7" />
-      </BotaoPadrao>
-      <BotaoPadrao
-        texto="imprimir"
-        cor="bg-blue-300 hover:!bg-blue-400"
-        @clique="gerarKpiDuracao">
-        <img
-          src="@/assets/icons/graph-b.svg"
-          alt=""
-          class="w-7 h-7" />
-      </BotaoPadrao>
+    <div class="flex gap-2 bg-gray-300 px-2 w-full py-1 items-center print:hidden justify-between">
+      <div class="flex gap-2">
+        <BotaoPadrao
+          texto="Excel"
+          @clique="buscarKpiDuracao"
+          :disabled="mostrarSemCards || carregando"
+          cor="bg-blue-300 hover:!bg-blue-400">
+          <img
+            src="@/assets/icons/excel-b.svg"
+            alt=""
+            class="w-7 h-7" />
+        </BotaoPadrao>
+        <BotaoPadrao
+          texto="imprimir"
+          cor="bg-blue-300 hover:!bg-blue-400"
+          :disabled="mostrarSemCards || carregando"
+          @clique="gerarKpiDuracao">
+          <img
+            src="@/assets/icons/graph-b.svg"
+            alt=""
+            class="w-7 h-7" />
+        </BotaoPadrao>
+      </div>
+      <div class="flex gap-2 items-end">
+        <span class="text-lg">Buscar por intervalo</span>
+        <AppFormInput id="data_inicio" type="date" v-model="data_inicio" label="Data inicial"/>
+        <AppFormInput id="data_final" type="date" v-model="data_final" label="Data final"/>
+        <div class="flex">
+          <BotaoPadrao cor="bg-blue-600 hover:!bg-blue-700" @clique="buscarDuracao()">
+            <img src="@/assets/icons/magnifier-w.svg" alt="" class="w-7 h-7">
+          </BotaoPadrao>
+        </div>
+      </div>
     </div>
     <div id="imprimir" class="h-du">
       <div class="bg-primaria-700 p-1 w-full text-white hidden print:flex justify-between items-center">
@@ -26,7 +40,13 @@
           src="@/assets/img/logoagcnovo.png"
           alt=""
           class="" />
-        <span>MÉDIA DE DURAÇÃO DE CARD POR ETAPA</span>
+        <div class="flex flex-col text-center">
+
+        <span>MÉDIA DE DURAÇÃO DE CARD POR ETAPA
+        </span>
+          <span v-if="data_inicio || data_final">Intervalo:  {{ data_inicio ? ' A partir: ' + $dayjs(data_inicio).format("DD/MM/YYYY") : "" }}
+          {{ data_final ? ' Até: ' + $dayjs(data_final).format("DD/MM/YYYY") : "" }}</span>
+        </div>
         <div class="flex flex-col">
           <span>Atualização: {{ $dayjs().format("DD/MM/YYYY") }}</span>
           <span
@@ -36,8 +56,8 @@
       </div>
     </div>
 
-    <div class="bg-white w-full relative" style="height: calc(100vh - 111px)">
-      <div class="mt-0 h-full w-full flex ">
+    <div class="bg-white w-full relative" style="height: calc(100vh - 121px)">
+      <div class="mt-0 h-full w-full flex " v-show="!mostrarSemCards">
         <client-only>
           <div
             ref="graDuracao"
@@ -45,22 +65,31 @@
             class="flex imprimindo-tabela"></div>
         </client-only>
       </div>
+      <div v-if="carregando" class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-gray-900/80 text-6xl font-bold text-white z-50 print:hidden">
+        <div class="square-circle-5"></div>
+        <span class="text-4xl font-bold">BUSCANDO DADOS, FAVOR AGUARDE</span>
+      </div>
+      <div class="w-full flex items-center text-gray-600 justify-center text-4xl font-bold h-full" v-show="mostrarSemCards">
+        <span>Sem cards encontrados durante esse intervalo!</span>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script>
 import BotaoPadrao from "~/components/Ui/Buttons/BotaoPadrao.vue";
 import gerarExcel from "~/functions/gerarExcel";
+import AppFormInput from "~/components/Ui/AppFormInput.vue";
 
 export default {
   name:"GraficoDuracaoEtapa",
-  components: { BotaoPadrao },
+  components: { AppFormInput, BotaoPadrao },
   data() {
     return {
       valores: [],
       largura: '100%',
+      data_inicio: null,
+      data_final: null,
       opcoes: {
         chart: {
           height: "100%",
@@ -210,7 +239,9 @@ export default {
           opacity: 1,
         },
       },
-      jaBuscou: false
+      jaBuscou: false,
+      mostrarSemCards: false,
+      carregando: false,
     };
   },
   async mounted() {
@@ -218,29 +249,40 @@ export default {
   },
   methods: {
     async buscarDuracao(){
-      let resp = await this.$axios.$get("/contratacao/kpi/duracao_card_etapa")
+      this.mostrarSemCards = false
+      this.carregando = true
+      let data_inicio = this.data_inicio
+      let data_final = this.data_final
+      data_inicio = data_inicio === '' ? null : data_inicio
+      data_final = data_final === '' ? null : data_final
+      let resp = await this.$axios.$get("/contratacao/kpi/duracao_card_etapa", { params: { data_inicio,  data_final}})
+      let { etapas, cards } = resp.dados
 
-      let { etapas } = resp.dados
+      if(cards.length > 0){
+        this.valores = [
+          { name: "Média", data: etapas.map((o) => parseFloat(o.media)), type: "bar" },
+          { name: "Previsto", data: etapas.map((o) => parseFloat(o.leadtime)), type: "line" },
+        ]
 
-      this.valores = [
-        { name: "Média", data: etapas.map((o) => parseFloat(o.media)), type: "bar" },
-        { name: "Previsto", data: etapas.map((o) => parseFloat(o.leadtime)), type: "line" },
-      ]
+        for (let etapa of etapas) {
+          let primeiraKey = Object.keys(etapa)[0]
+          this.opcoes.xaxis.categories.push(etapa[primeiraKey])
+        }
 
-      for(let etapa of etapas){
-        let primeiraKey = Object.keys(etapa)[0]
-        this.opcoes.xaxis.categories.push(etapa[primeiraKey])
+        let valores = this.valores
+        let opcoes = Object.assign({}, this.opcoes)
+
+        if (this.jaBuscou === false) {
+          await this.criarGrafico(valores, opcoes)
+          this.jaBuscou = true
+        } else {
+          await this.atualizarGrafico(valores, opcoes)
+        }
+      }else{
+        this.mostrarSemCards = true
       }
 
-      let valores = this.valores
-      let opcoes = Object.assign({}, this.opcoes)
-
-      if (this.jaBuscou === false) {
-        await this.criarGrafico(valores, opcoes)
-        this.jaBuscou = true
-      } else {
-        await this.atualizarGrafico(valores, opcoes)
-      }
+      this.carregando = false
     },
     async criarGrafico(valores, opcoes) {
       const chartContainer = this.$refs.graDuracao
@@ -357,6 +399,56 @@ export default {
 
   #imprimir > * {
     page-break-inside: avoid;
+  }
+}
+
+.square-circle-5 {
+  width: 65px;
+  aspect-ratio: 1;
+  position: relative;
+}
+
+.square-circle-5:before,
+.square-circle-5:after {
+  content: "";
+  position: absolute;
+  border-radius: 50px;
+  box-shadow: 0 0 0 3px inset #fff;
+  animation: sc5 2.5s infinite;
+}
+
+.square-circle-5:after {
+  animation-delay: -1.25s;
+  border-radius: 0;
+}
+
+@keyframes sc5 {
+  0% {
+    inset: 0 35px 35px 0;
+  }
+  12.5% {
+    inset: 0 35px 0 0;
+  }
+  25% {
+    inset: 35px 35px 0 0;
+  }
+  37.5% {
+    inset: 35px 0 0 0;
+  }
+  50% {
+    inset: 35px 0 0 35px;
+  }
+  62.5% {
+    inset: 0 0 0 35px;
+  }
+  75% {
+    inset: 0 0 35px 35px;
+  }
+  87.5% {
+    inset: 0 0 35px 0;
+  }
+  100% {
+    inset: 0 35px 35px 0;
   }
 }
 
