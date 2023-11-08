9d702c7b57d528b30f2f62547d671f4c37d7d0f8
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2023-11-08 16:05:00 -0300

    Corrigi só para mostrar o que cada usuario tem permissão

--- pages/Administracao/PSP/index.vue
+++ pages/Administracao/PSP/index.vue
@@ -1,39 +1,63 @@
 <template>
-  <div class="w-full grid grid-cols-1 h-fit gap-y-2">
-    <div
-      class="border grid grid-cols-4 space-x-3 border-1 shadow border-gray-300 p-2 rounded bg-white">
-      <ButtonNavegacao titulo="Solicitações" cor="bg-[#264653]" link="psp/solicitacoes">
-        <img src="@/assets/icons/travel-w.svg" alt="" class="w-8 h-8">
-      </ButtonNavegacao>
-      <ButtonNavegacao titulo="Aprovar PSP" cor="bg-[#264653]" link="psp/aprovacao">
-        <img src="@/assets/icons/check-circle-w.svg" alt="" class="w-8 h-8">
-      </ButtonNavegacao>
-      <ButtonNavegacao titulo="Configurações" cor="bg-[#264653]" link="psp/configuracoes">
-        <img src="@/assets/icons/cog-w.svg" alt="" class="w-8 h-8">
-      </ButtonNavegacao>
-    </div>
-  </div>
+	<div class="w-full grid grid-cols-1 h-fit gap-y-2">
+		<div
+			class="border grid grid-cols-4 space-x-3 border-1 shadow border-gray-300 p-2 rounded bg-white">
+			<ButtonNavegacao
+				titulo="Solicitações"
+				cor="bg-[#264653]"
+				link="psp/solicitacoes">
+				<img
+					src="@/assets/icons/travel-w.svg"
+					alt=""
+					class="w-8 h-8" />
+			</ButtonNavegacao>
+			<ButtonNavegacao
+				titulo="Aprovar PSP"
+				cor="bg-[#264653]"
+				link="psp/aprovacao"
+				v-if="
+					$auth.user.permissoes.includes('aprovar_psp_coordenador') ||
+					$auth.user.permissoes.includes('aprovar_psp_gaf') ||
+					$auth.user.permissoes.includes('aprovar_psp_gerente') ||
+					$auth.user.permissoes.includes('aprovar_psp_rh')
+				">
+				<img
+					src="@/assets/icons/check-circle-w.svg"
+					alt=""
+					class="w-8 h-8" />
+			</ButtonNavegacao>
+			<ButtonNavegacao
+				titulo="Configurações"
+				cor="bg-[#264653]"
+				link="psp/configuracoes"
+				v-if="$auth.user.permissoes.includes('gerenciamento_psp')">
+				<img
+					src="@/assets/icons/cog-w.svg"
+					alt=""
+					class="w-8 h-8" />
+			</ButtonNavegacao>
+		</div>
+	</div>
 </template>
 
 <script>
-import ButtonNavegacao from "~/components/Shared/ButtonNavegacao.vue"
-import AppTabs from "~/components/Ui/AppTabs.vue"
-import AppFormInput from "~/components/Ui/AppFormInput.vue"
-import BotaoPadrao from "~/components/Ui/Buttons/BotaoPadrao.vue"
+	import ButtonNavegacao from "~/components/Shared/ButtonNavegacao.vue"
+	import AppTabs from "~/components/Ui/AppTabs.vue"
+	import AppFormInput from "~/components/Ui/AppFormInput.vue"
+	import BotaoPadrao from "~/components/Ui/Buttons/BotaoPadrao.vue"
 
-export default {
-  name: "index",
-  components: {
-    ButtonNavegacao,
-    AppTabs,
-    AppFormInput,
-    BotaoPadrao,
-  },
-  data() {
-    return {
-    }
-  },
-}
+	export default {
+		name: "index",
+		components: {
+			ButtonNavegacao,
+			AppTabs,
+			AppFormInput,
+			BotaoPadrao,
+		},
+		data() {
+			return {}
+		},
+	}
 </script>
 
 <style scoped></style>
